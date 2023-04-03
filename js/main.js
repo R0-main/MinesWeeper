e6dfecb0bd3d8aaa7a094e9b529f0cea3c68eedc
Romain <127017159+AI-Romain@users.noreply.github.com>
2023-04-03 17:21:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 let map = {};
 let started = false;
-let bombs = 10;
-let needToClick = (10 * 10) - bombs - 1;
+let bombs = () => {
+    return document.getElementById("bomb-entry").value;
+};
+let gridSize = () => {
+    return {
+        x: document.getElementById("grid-entry").value,
+        y: document.getElementById("grid-entry").value
+    };
+};
+let needToClick = ((gridSize().x * gridSize().y) - bombs()) - 1;
 let firstCoords = {
     x: 0,
     y: 0
 };
+
+
 let findedCases = [];
 let flagedCases = [];
 let bombCoords = [];
+let renderBomb = () => {
+    return document.getElementById("render-bomb").checked;
+};;
 
 
 function leftClick(x, y) {
@@ -23,7 +36,7 @@ function leftClick(x, y) {
 
     // check if is bomb 
     if (bombCoords.includes(`${x}-${y}`)) {
-        return endGame();
+        return endGame(x, y);
     }
 
     if (needToClick == 0 && !findedCases.includes(`${x}-${y}`)) {
@@ -57,7 +70,8 @@ function resetCase(x, y) {
 }
 
 function setCaseTexture(x, y, texture) {
-    document.getElementById(`${x}-${y}`).src = `assets/${texture}.png`;
+    if (document.getElementById(`${x}-${y}`))
+        document.getElementById(`${x}-${y}`).src = `assets/${texture}.png`;
 }
 
 // prevent context menu appearing
@@ -72,7 +86,7 @@ function initGame(x, y) {
     firstCoords.x = x;
     firstCoords.y = y;
 
-    for (let i = 0; i < bombs; i++) {
+    for (let i = 0; i < bombs(); i++) {
         randomCoords();
     }
 
@@ -94,14 +108,14 @@ function findAdjacentCases(x, y) {
 
     for (let row = x - 1; row <= x + 1; row++) {
         for (let col = y - 1; col <= y + 1; col++) {
-            if (row < 0 || row >= 10 || col < 0 || col >= 10) {
+            if (row < 0 || row >= gridSize().x || col < 0 || col >= gridSize().y) {
                 continue;
             }
-
             if (row === x && col === y) {
                 continue;
             }
-            if (checkMap(x, y, 'any') == 0) {
+
+            if (checkMap(x, y, 'any') == 0 && !flagedCases.includes(`${row}-${col}`)) {
                 adjacentCases.push({ x: row, y: col });
             }
         }
@@ -110,10 +124,16 @@ function findAdjacentCases(x, y) {
     return adjacentCases;
 }
 
+function updateScore() {
+    document.getElementById(`score`).innerHTML = `Score : ${((gridSize().x * gridSize().y) - bombs()) - needToClick}/${(gridSize().x * gridSize().y) - bombs()}`;
+}
+
 function revealCase(x, y) {
+    if (flagedCases.includes(`${x}-${y}`))
+        return;
     if (!findedCases.includes(`${x}-${y}`)) {
         needToClick--;
-        document.getElementById(`score`).innerHTML = `Score : ${((10 * 10) - 1) - needToClick}`;
+        updateScore();
     }
 
     if (checkMap(x, y, 'any') != 0 || findedCases.includes(`${x}-${y}`)) {
@@ -132,7 +152,7 @@ function revealCase(x, y) {
                 if (i === 0 && j === 0) continue;
                 const adjacentX = x + i;
                 const adjacentY = y + j;
-                if (adjacentX < 0 || adjacentX > 9 || adjacentY < 0 || adjacentY > 9) continue; // check boundaries
+                if (adjacentX < 0 || adjacentX > gridSize().x - 1 || adjacentY < 0 || adjacentY > gridSize().y - 1) continue; // check boundaries
                 revealCase(adjacentX, adjacentY);
             }
         }
@@ -141,20 +161,22 @@ function revealCase(x, y) {
 
 
 function randomCoords() {
-    let x = getRandomInt(9);
-    let y = getRandomInt(9);
+    let x = getRandomInt(gridSize().x) - 1;
+    let y = getRandomInt(gridSize().y) - 1;
 
     // check if the generated coordinates are within the forbidden area
     const isForbidden = x >= firstCoords.x - 1 && x <= firstCoords.x + 1
         && y >= firstCoords.y - 1 && y <= firstCoords.y + 1;
 
     if (isForbidden || bombCoords.includes(`${x}-${y}`)) {
-        // regenerate coordinates if they are in the forbidden area or already contain a bomb
         randomCoords();
         return;
+        // regenerate coordinates if they are in the forbidden area or already contain a bomb
     }
     saveMapAs(x, y, 'bomb');
-    setCaseTexture(x, y, 'case_bomb');
+    if (renderBomb()) {
+        setCaseTexture(x, y, 'case_bomb');
+    }
     bombCoords.push(`${x}-${y}`);
 }
 
@@ -174,8 +196,31 @@ function getMapCase(x, y) {
     return parseInt(map[`${x}-${y}`]) || 0;
 }
 
-function endGame() {
-    location.reload();
+function endGame(x, y) {
+
+    flagedCases.forEach((flag) => {
+        bombCoords.forEach((bomb) => {
+            let flagX = parseInt(flag.split('-')[0]);
+            let flagY = parseInt(flag.split('-')[1]);
+
+            if (flag != bomb)
+                setCaseTexture(flagX, flagY, 'case_not_bomb');
+        });
+    });
+    bombCoords.forEach((bomb) => {
+        let bombX = parseInt(bomb.split('-')[0]);
+        let bombY = parseInt(bomb.split('-')[1]);
+
+        if (x == bombX && y == bombY) {
+            setCaseTexture(x, y, 'case_red_bomb');
+        }
+        else
+            setCaseTexture(bombX, bombY, 'case_bomb');
+    });
+
+
+
+    //location.reload();;
 }
 
 function getAllBombs() {
@@ -200,3 +245,41 @@ function win() {
     alert('You win !');
 }
 
+function generateGrid(x, y) {
+    // Sélectionnez l'élément parent pour ajouter la grille
+    var parent = document.getElementById("main-grid");
+
+    // Créez une boucle pour générer la grille avec les éléments HTML
+    for (var i = 0; i < y; i++) {
+        // Créez un élément de colonne
+        var column = document.createElement("div");
+        column.id = "column-" + i;
+        column.className = "column";
+
+        // Ajoutez une colonne à l'élément parent
+        parent.appendChild(column);
+
+        for (var j = 0; j < x; j++) {
+            // Créez un élément de case
+            var cell = document.createElement("a");
+            cell.setAttribute('onclick', `leftClick(${j}, ${i})`);
+            cell.setAttribute('oncontextmenu', `rightClick(${j}, ${i})`);
+            // Ajoutez une case à la colonne
+            column.appendChild(cell);
+
+            // Créez un élément d'image pour la case
+            var image = document.createElement("img");
+            image.id = `${j}-${i}`;
+            image.src = "assets/case.png";
+
+            // Ajoutez l'image à la case
+            cell.appendChild(image);
+        }
+    }
+};
+
+function generateArea() {
+    generateGrid(gridSize().x, gridSize().y);
+    document.getElementById('start-btn').setAttribute("disabled", "true");
+    updateScore();
+}
